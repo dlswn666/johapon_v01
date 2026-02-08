2e38d62d39bb0be6e4481ba8a91ed5c427a18d60
dlswn666 <dlswn666@gmail.com>
2026-02-08 18:15:44 +0900

    랜딩페이지 이상 내용 삭제

--- app/_lib/widgets/landing/index.ts
+++ app/_lib/widgets/landing/index.ts
@@ -1,15 +1,3 @@
-// 기존 컴포넌트
 export { CrossfadeBackground } from './CrossfadeBackground';
 export { LoginForm } from './LoginForm';
-
-// 새 피그마 기반 컴포넌트
-export { LandingHeader } from './LandingHeader';
-export { LandingHero } from './LandingHero';
-export { LandingContentGrid } from './LandingContentGrid';
-export { LandingFooter } from './LandingFooter';
-
-// 메인 랜딩 페이지 (새 디자인)
-export { LandingPageNew as LandingPage } from './LandingPageNew';
-
-// 기존 랜딩 페이지 (레거시)
-export { LandingPage as LandingPageLegacy } from './LandingPage';
+export { LandingPage } from './LandingPage';
